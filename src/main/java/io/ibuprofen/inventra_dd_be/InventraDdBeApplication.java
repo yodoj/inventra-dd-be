b5111cb550b206b895e101699f5741db67d97b14
Nadira Aliya Nashwa <nadiraalia4@gmail.com>
2026-02-15 12:53:36 +0700

    chore: initialize project

---
+++ src/main/java/io/ibuprofen/inventra_dd_be/InventraDdBeApplication.java
@@ -0,0 +1,13 @@
+package io.ibuprofen.inventra_dd_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventraDdBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventraDdBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/ibuprofen/inventra_dd_be/InventraDdBeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ibuprofen.inventra_dd_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventraDdBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
