c62160793f7cb349ce2887fecf6a09f8c7057d62
Indah Cahya Puspitahati <indah.cahya31@ui.ac.id>
2026-02-21 22:21:01 +0700

    Feat/indah: fixing directory structure

---
+++ src/main/java/io/ibuprofen/inventra_dd_be/Profile/restdto/request/LoginRequest.java
@@ -0,0 +1,13 @@
+package io.ibuprofen.inventra_dd_be.Profile.restdto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+}
