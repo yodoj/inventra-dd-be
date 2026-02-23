b8f3ff2acff7bee9c1f9c74136c965b8ac2b27ab
Patricia Gloria Sujatmoko Silaban <patricia.gloria@ui.ac.id>
2026-02-23 14:22:37 +0700

    Feat/cia : read and edit profile (self) done

---
+++ src/main/java/io/ibuprofen/inventra_dd_be/Profile/restdto/request/UpdatePasswordRequestDTO.java
@@ -0,0 +1,23 @@
+package io.ibuprofen.inventra_dd_be.Profile.restdto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UpdatePasswordRequestDTO {
+    
+    @NotBlank(message = "Password saat ini tidak boleh kosong")
+    private String currentPassword;
+
+    @NotBlank(message = "Password baru tidak boleh kosong")
+    private String newPassword;
+
+    @NotBlank(message = "Konfirmasi password tidak boleh kosong")
+    private String confirmPassword;
+}
