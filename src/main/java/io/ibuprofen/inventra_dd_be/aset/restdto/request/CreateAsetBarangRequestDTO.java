bdf8714c6815e1babcf26dc998a4011217db969c
Ridya Azizah Khayyira Mumtaz <ridya.azizah@ui.ac.id>
2026-02-27 19:19:32 +0700

    Merge request CRUD Mengelola Aset Feat/ridya

---
+++ src/main/java/io/ibuprofen/inventra_dd_be/aset/restdto/request/CreateAsetBarangRequestDTO.java
@@ -0,0 +1,39 @@
+package io.ibuprofen.inventra_dd_be.Aset.restdto.request;
+
+import io.ibuprofen.inventra_dd_be.Aset.model.KategoriAset;
+import io.ibuprofen.inventra_dd_be.Aset.model.StatusAset;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateAsetBarangRequestDTO {
+    @NotBlank(message = "Nama aset tidak boleh kosong")
+    private String namaAset;
+
+    @NotBlank(message = "Link gambar tidak boleh kosong")
+    private String gambarUrlAset;
+
+    @NotNull(message = "Kategori aset tidak boleh boleh kosong")
+    private KategoriAset kategoriAset;
+
+    @NotNull(message = "Status aset tidak boleh boleh kosong")
+    private StatusAset statusAset;
+
+    private String keteranganAset;
+
+    @NotBlank(message = "Merk aset tidak boleh kosong")
+    private String merkAset;
+
+    @NotNull(message = "Qty aset tidak boleh kosong")
+    private Integer qtyAset;
+
+    @NotBlank(message = "Lokasi aset tidak boleh kosong")
+    private String lokasiAset;
+
+    private String unit;
+}
